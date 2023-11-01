de481ebcb6c8090a15c2bb5e9ae0a9ac192762f9
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-01 08:46:53 -0700

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 
 apt-get update
 apt-get -y install nginx
-# Install Nginx if it not already installed
+# Install Nginx if it is not already installed
 mkdir -p /data/web_static/releases/test/
 # Create the folder /data/ if it doesn’t already exist
 # Create the folder /data/web_static/ if it doesn’t already exist
@@ -22,8 +22,8 @@ echo '<html>
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Create a symbolic link /data/web_static/current linked to the...
 # .../data/web_static/releases/test/ folder. If the symbolic link already...
-# ...exists, it should be deleted and recreated every time the script is ran.
-chown -hR ubuntu:ubuntu /data/
+# ...exists, it should be deleted and recreated every time the script is run.
+chown -R ubuntu:ubuntu /data/
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can
 # assume this user and group exist). This should be recursive; everything
 # inside should be created/owned by this user/group.
@@ -33,3 +33,4 @@ sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t
 # (ex: https://mydomainname.tech/hbnb_static).
 # Don’t forget to restart Nginx after updating the configuration:
 service nginx restart
+
