a602e5e678ea6c8045e5434510601123ff44b097
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-03 11:54:38 -0700

    deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,15 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
-With Fabric, creates a tgz archive
+With Facric , creates a tgz archive
 from web_static content folder
 """
 
-from fabric.api import local, put, run, sudo
-from os.path import exists, isdir
+from fabric.api import env, local, put, run
 from datetime import datetime
+from os.path import exists, isdir
+env.hosts = ['35.153.232.148', '54.234.35.137']
+env.user = 'scool'
+env.key_filename = '~/.ssh/id_rsa'
+
 
 def do_pack():
-    """Creates a tgz archive using Fabric"""
+    """Creates a tgz archive using fabric"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
@@ -20,29 +24,31 @@ def do_pack():
     except Exception as ex:
         return None
 
+
 def do_deploy(archive_path):
-    """Deploy web static with Fabric"""
+    """deploy web static with fabric"""
     if exists(archive_path) is False:
         return False
 
     try:
         filename = archive_path.split("/")[-1]
-        no_extension = filename.split(".")[0]
+        no_excep = filename.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        sudo('mkdir -p {}{}/'.format(path, no_extension))
-        sudo('tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_extension))
-        sudo('rm /tmp/{}'.format(filename))
-        sudo('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_extension))
-        sudo('rm -rf {}{}/web_static'.format(path, no_extension))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}{}/ /data/web_static/current'.format(path, no_extension))
+        run('sudo mkdir -p {}{}/'.format(path, no_excep))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
+        run('sudo rm /tmp/{}'.format(filename))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_excep))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
         return True
     except BaseException:
         return False
 
+
 def deploy():
-    """Do pack and do deploy"""
+    """ do path an do deploy"""
     archive_path = do_pack()
     if archive_path is None:
         return False
