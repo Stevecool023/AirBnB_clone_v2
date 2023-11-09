ebb835add92bdd3aab0aa70c5f0ef8f5d17d4241
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-09 13:47:44 -0800

    routes

--- web_static/agrimarket/routes.py
+++ web_static/agrimarket/routes.py
@@ -1,36 +1,25 @@
-#!/usr/bin/env python3
-
 from flask import render_template
-from app import app
+from app import app, db
+from app.models import Product, BlogPost, Equipment
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
-
 @app.route('/products')
 def products():
-    products_data = [
-        {"name": "Product 1", "description": "Description 1"},
-        {"name": "Product 2", "description": "Description 2"},
-        # Add more product data as needed
-    ]
+    # Fetch products from the database
+    products_data = Product.query.all()
     return render_template('products.html', products=products_data)
 
 @app.route('/blog')
 def blog():
-    blog_data = [
-        {"title": "Article 1", "content": "Content of Article 1"},
-        {"title": "Article 2", "content": "Content of Article 2"},
-        # Add more blog articles as needed
-    ]
+    # Fetch blog posts from the database
+    blog_data = BlogPost.query.all()
     return render_template('blog.html', blog=blog_data)
 
 @app.route('/equipment')
 def equipment():
-    equipment_data = [
-        {"name": "Equipment 1", "description": "Description 1"},
-        {"name": "Equipment 2", "description": "Description 2"},
-        # Add more equipment data as needed
-    ]
+    # Fetch equipment from the database
+    equipment_data = Equipment.query.all()
     return render_template('equipment.html', equipment=equipment_data)
